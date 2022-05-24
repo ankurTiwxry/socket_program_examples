54857a299890b8cf16822b821f16fdccde1092d4
Ankur <a.tiwary@cruden.com>
2022-05-24 14:30:57 +0200

    Added a basic template log class

--- applications/sp_backend/sp_backend/include/logging.h
+++ applications/sp_backend/sp_backend/include/logging.h
@@ -1,4 +1,38 @@
 #pragma once
+
+#include <iostream>
+#include <string>
+#include <fstream>
+
+template<typename T>
 class logging {
+
+public:
+
+  // ctor
+  logging() {
+    output_file.open("../log.txt");
+  }
+
+  // dtor
+  ~logging() {
+    output_file.close();
+  }
+
+  // log in console window 
+  void Log(const T& msg) {
+    std::cout << msg << std::endl;
+  }
+
+  // log to file
+  void LogToFile(const T& msg) {
+    output_file << msg << std::endl;
+    std::cout << "LOG: " << msg << std::endl;
+  }
+
+
+private:
+  std::ofstream output_file;
+
 };
 
--- applications/sp_backend/sp_backend/source/main.cpp
+++ applications/sp_backend/sp_backend/source/main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
+#include <string>
+#include "../include/logging.h"
 
 int main() {
-
+  
+  // logging types
+  logging <int>log_int;
+  logging <std::string>log_str;
+    
+  
+  // keep console window active
+  std::cin.get();
 }
