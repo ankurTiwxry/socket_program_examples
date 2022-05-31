a86f158ff55e5ebc3c3b136b3a28e908cd8461f8
Ankur <a.tiwary@cruden.com>
2022-05-31 14:47:45 +0200

    Socket initializer bug fixed

--- applications/sp_backend/sp_backend/include/socket_initializer.h
+++ applications/sp_backend/sp_backend/include/socket_initializer.h
@@ -3,17 +3,13 @@
 #include "common_headers.h"
 #include "logging.h"
 
-struct udp_socket_parameters {
+struct udp_socket_parameters {\
+  // NOTE: For some reason, if you define the structs after the int, the last defined one will try to write to unallocated memory
+  // causes error : Heap Corruption Detected: after Normal block (#191) at <some_hex_address> CRT detected that the application wrote to memory after end of heap bugger
+  struct sockaddr_in m_recv_sock_addr;
+  struct sockaddr_in m_send_sock_addr;
   int m_send_socket_fd;
   int m_recv_socket_fd;
-  struct sockaddr_in m_send_sock_addr;
-  struct sockaddr_in m_recv_sock_addr;
-
-  /*udp_socket_parameters(int send_socket_fd, struct sockaddr_in send_sock_addr, int recv_socket_fd, struct sockaddr_in recv_sock_addr) 
-  : m_send_socket_fd{send_socket_fd}, m_send_sock_addr{send_sock_addr}, m_recv_socket_fd{recv_socket_fd}, m_recv_sock_addr{recv_sock_addr} 
-  {
-    logger::Log("udp_socket_parameters constructor called");
-  }*/
 };
 
 class socket_initializer 
@@ -31,10 +27,8 @@ private:
   // variables
   WSAData ws;
   
-  // dynamic list of sockets with all necessary parameters
+  // dynamic list of udp sockets with all necessary parameters
   std::vector<udp_socket_parameters> udp_sockets;
-
-  // temporary variables
 
   // functions
   // wrapper functions
@@ -44,6 +38,5 @@ private:
   void CreateUdpSocket(const std::string& name, const int& port, const std::string& ip_address);
   void CreateUdpSendSocket(const int& port, const std::string& ip_address);
   void CreateUdpRecvSocket(const int& port);
-
 };
 
--- applications/sp_backend/sp_backend/source/socket_initializer.cpp
+++ applications/sp_backend/sp_backend/source/socket_initializer.cpp
@@ -3,19 +3,13 @@
 socket_initializer::socket_initializer()
 {
   // set up sockets array
+  udp_sockets.clear();
+  udp_sockets.resize(0);
+  
   // wsa variable check
-  logger::Log(udp_sockets.size());
   logger::CheckValidity("WSA Startup ", WSAStartup(MAKEWORD(2, 2), &ws));
+  
   SetupUiSockets();
-
-  logger::Log(udp_sockets.size());
-  for (unsigned int i = 0; i < udp_sockets.size(); i++) {
-  // temp func : check assigned values
-    logger::LogSpace();
-    logger::LogInline("Udp Socket     : "); logger::Log(i+1);  
-    logger::LogInline("Send Socket FD : "); logger::Log(udp_sockets[i].m_send_socket_fd);
-    logger::LogInline("Recv Socket FD : "); logger::Log(udp_sockets[i].m_recv_socket_fd);
-  }
 }
 
 socket_initializer::~socket_initializer() {
@@ -28,12 +22,12 @@ void socket_initializer::SetupUiSockets() {
   CreateUdpSocket("Wpf Ui 1", 25005, "127.0.0.1"); 
 }
 
-
 void socket_initializer::CreateUdpSocket(const std::string& name, const int& port, const std::string& ip_address) {
-  udp_sockets.resize(udp_sockets.size() + 1);
   logger::Log("Creating UDP socket to send and receive from " + name);
-  CreateUdpRecvSocket(25005);
-  //CreateUdpSendSocket(25006, ip_address);
+  
+  udp_sockets.resize(udp_sockets.size() + 1); // resize array by 1
+  CreateUdpRecvSocket(port+1);
+  CreateUdpSendSocket(port, ip_address);
 }
 
 void socket_initializer::CreateUdpRecvSocket(const int& port) {
@@ -42,20 +36,21 @@ void socket_initializer::CreateUdpRecvSocket(const int& port) {
   logger::CheckValidity("Recv : socket()", udp_sockets[i].m_recv_socket_fd);
 
   udp_sockets[i].m_recv_sock_addr.sin_family = AF_INET;
-  udp_sockets[i].m_recv_sock_addr.sin_port = htons(25005);
+  udp_sockets[i].m_recv_sock_addr.sin_port = htons(port);
   udp_sockets[i].m_recv_sock_addr.sin_addr.s_addr = INADDR_ANY;
-  memset(&(udp_sockets[i].m_recv_sock_addr.sin_zero), 0, sizeof(udp_sockets[i].m_recv_sock_addr));
+  memset(&(udp_sockets[i].m_recv_sock_addr.sin_zero), 0, sizeof(udp_sockets[i].m_recv_sock_addr)); // TODO : FIX BUG : This line is causing the issue
 
-  logger::Log(logger::CheckValidity("Recv : bind()", bind(udp_sockets[i].m_recv_socket_fd, (struct sockaddr*)&udp_sockets[i].m_recv_sock_addr, sizeof(udp_sockets[i].m_recv_sock_addr))));
+  logger::CheckValidity("Recv : bind()", bind(udp_sockets[i].m_recv_socket_fd, (struct sockaddr*)&udp_sockets[i].m_recv_sock_addr, sizeof(udp_sockets[i].m_recv_sock_addr)));
 }
 
 void socket_initializer::CreateUdpSendSocket(const int& port, const std::string& ip_address) {
   int i = udp_sockets.size() - 1;
+
   udp_sockets[i].m_send_socket_fd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
   logger::CheckValidity("Send : socket()", udp_sockets[i].m_send_socket_fd);
 
   udp_sockets[i].m_send_sock_addr.sin_family = AF_INET;
-  udp_sockets[i].m_send_sock_addr.sin_port = htons(25006);
+  udp_sockets[i].m_send_sock_addr.sin_port = htons(port);
   udp_sockets[i].m_send_sock_addr.sin_addr.s_addr = inet_addr(ip_address.c_str());
   memset(&(udp_sockets[i].m_send_sock_addr.sin_zero), 0, sizeof(udp_sockets[i].m_send_sock_addr));
 }
